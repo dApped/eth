85d7b610e239c6c3905d20fec1660678f6c4b8e8
dapped <dapped@protonmail.com>
2021-05-13 18:02:40 +1000

    Use base unique instead of dplyr::distinct

--- R/get_txs.R
+++ R/get_txs.R
@@ -28,7 +28,7 @@
 #' limit of 5 requests/sec._
 #' @keywords Ethereum, transaction, blockchain, cryptocurrency, crypto, ETH
 #' @importFrom jsonlite fromJSON
-#' @importFrom dplyr filter as_tibble mutate distinct select matches %>%
+#' @importFrom dplyr filter as_tibble mutate select matches %>%
 #' @importFrom lubridate with_tz now
 #' @importFrom gmp as.bigz
 #' @export
@@ -155,7 +155,7 @@ get_txs <- function(address, api_key, internal=FALSE,
     }
   }
   now <- lubridate::now(tzone='UTC')
-  txs <- txs %>% dplyr::distinct()
+  txs <- unique(txs)
   if(isTRUE(no_errors) && type %in% c('normal', 'internal'))
     txs <- dplyr::filter(txs, isError=='0')
   attr(txs, 'address') <- config$address
